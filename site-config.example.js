83d5429144ff8375c080622b4b0937988b9a75a1
Isaac Houngue <i.houngue@instadeep.com>
2026-06-12 22:00:47 +0200

    Delete unwanted files

--- site-config.example.js
+++
@@ -1,6 +0,0 @@
-// Copy to site-config.js and add your Web3Forms access key.
-// Site copy: content.js | Shared helpers: shared.js | Styles: variables.css
-window.SITE_CONFIG = {
-    web3formsAccessKey: 'YOUR_ACCESS_KEY_HERE',
-    trackVisits: true,
-};
